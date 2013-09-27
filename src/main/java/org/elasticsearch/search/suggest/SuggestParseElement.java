openeuler-mirror/poissonsearch-oss

Add ShardId and Index to SuggestionContext
Suggesters might need access to the shard they run on as well as the
index they operate on. This patch adds indexname and shard ID to the
SuggestionContext

Closes #3199

src/main/java/org/elasticsearch/search/suggest/SuggestParseElement.java
@@ -18,6 +18,8 @@
  */
 package org.elasticsearch.search.suggest;
 
+import org.elasticsearch.search.SearchShardTarget;
+
 import java.io.IOException;
 
 import org.apache.lucene.util.BytesRef;
@@ -32,7 +34,7 @@ import org.elasticsearch.search.suggest.SuggestionSearchContext.SuggestionContex
 /**
  *
  */
-public class SuggestParseElement implements SearchParseElement {
+public final class SuggestParseElement implements SearchParseElement {
     private Suggesters suggesters;
 
     @Inject
@@ -42,11 +44,11 @@ public class SuggestParseElement implements SearchParseElement {
 
     @Override
     public void parse(XContentParser parser, SearchContext context) throws Exception {
-        SuggestionSearchContext suggestionSearchContext = parseInternal(parser, context.mapperService());
+        SuggestionSearchContext suggestionSearchContext = parseInternal(parser, context.mapperService(), context.shardTarget().index(), context.shardTarget().shardId());
         context.suggest(suggestionSearchContext);
     }
 
-    public SuggestionSearchContext parseInternal(XContentParser parser, MapperService mapperService) throws IOException {
+    public SuggestionSearchContext parseInternal(XContentParser parser, MapperService mapperService, String index, int shardId) throws IOException {
         SuggestionSearchContext suggestionSearchContext = new SuggestionSearchContext();
         BytesRef globalText = null;
         String fieldName = null;
@@ -81,7 +83,12 @@ public class SuggestParseElement implements SearchParseElement {
                             throw new ElasticSearchIllegalArgumentException("Suggester[" + fieldName + "] not supported");
                         }
                         final SuggestContextParser contextParser = suggesters.get(fieldName).getContextParser();
-                        parseAndVerify(parser, mapperService, suggestionSearchContext, globalText, suggestionName, suggestText, contextParser);
+                        SuggestionContext suggestion = contextParser.parse(parser, mapperService);
+                        suggestion.setText(suggestText);
+                        suggestion.setShard(shardId);
+                        suggestion.setIndex(index);
+                        SuggestUtils.verifySuggestion(mapperService, globalText, suggestion);
+                        suggestionSearchContext.addSuggestion(suggestionName, suggestion);
                     }
                 }
             }
@@ -89,13 +96,6 @@ public class SuggestParseElement implements SearchParseElement {
         return suggestionSearchContext;
     }
 
-    public void parseAndVerify(XContentParser parser, MapperService mapperService, SuggestionSearchContext suggestionSearchContext,
-            BytesRef globalText, String suggestionName, BytesRef suggestText, SuggestContextParser suggestParser ) throws IOException {
-        SuggestionContext suggestion = suggestParser.parse(parser, mapperService);
-        suggestion.setText(suggestText);
-        SuggestUtils.verifySuggestion(mapperService, globalText, suggestion);
-        suggestionSearchContext.addSuggestion(suggestionName, suggestion);
-    }
 
    
 }

mute/elasticsearch

src/main/java/org/elasticsearch/search/suggest/SuggestParseElement.java
@@ -18,8 +18,6 @@
  */
 package org.elasticsearch.search.suggest;
 
-import java.io.IOException;
-
 import org.apache.lucene.util.BytesRef;
 import org.elasticsearch.ElasticSearchIllegalArgumentException;
 import org.elasticsearch.common.inject.Inject;
@@ -29,6 +27,7 @@ import org.elasticsearch.search.SearchParseElement;
 import org.elasticsearch.search.internal.SearchContext;
 import org.elasticsearch.search.suggest.SuggestionSearchContext.SuggestionContext;
 
+import java.io.IOException;
 import java.util.Map;
 
 import static com.google.common.collect.Maps.newHashMap;
@@ -36,7 +35,7 @@ import static com.google.common.collect.Maps.newHashMap;
 /**
  *
  */
-public class SuggestParseElement implements SearchParseElement {
+public final class SuggestParseElement implements SearchParseElement {
     private Suggesters suggesters;
 
     @Inject
@@ -46,11 +45,11 @@ public class SuggestParseElement implements SearchParseElement {
 
     @Override
     public void parse(XContentParser parser, SearchContext context) throws Exception {
-        SuggestionSearchContext suggestionSearchContext = parseInternal(parser, context.mapperService());
+        SuggestionSearchContext suggestionSearchContext = parseInternal(parser, context.mapperService(), context.shardTarget().index(), context.shardTarget().shardId());
         context.suggest(suggestionSearchContext);
     }
 
-    public SuggestionSearchContext parseInternal(XContentParser parser, MapperService mapperService) throws IOException {
+    public SuggestionSearchContext parseInternal(XContentParser parser, MapperService mapperService, String index, int shardId) throws IOException {
         SuggestionSearchContext suggestionSearchContext = new SuggestionSearchContext();
         BytesRef globalText = null;
         String fieldName = null;
@@ -100,7 +99,8 @@ public class SuggestParseElement implements SearchParseElement {
             for (Map.Entry<String, SuggestionContext> entry : suggestionContexts.entrySet()) {
                 String suggestionName = entry.getKey();
                 SuggestionContext suggestionContext = entry.getValue();
-
+                suggestionContext.setShard(shardId);
+                suggestionContext.setIndex(index);
                 SuggestUtils.verifySuggestion(mapperService, globalText, suggestionContext);
                 suggestionSearchContext.addSuggestion(suggestionName, suggestionContext);
             }